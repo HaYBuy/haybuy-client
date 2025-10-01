9e27b63c800c091d19c97b2ca94a180ef47e23c6
Setsiri <setsiriaunsiam@gmail.com>
2025-10-01 15:57:23 +0700

    add assets+utils

---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,13 @@
+class TText {
+  // Onboarding Texts
+  static const String tOnboardingTitle1 = 'Find Products';
+  static const String tOnboardingTitle2 = 'Fast Delivery';
+  static const String tOnboardingTitle3 = 'Easy Payment';
+
+  static const String tOnboardingSubTitle1 =
+      'Easily find your products in our app with just a few taps.';
+  static const String tOnboardingSubTitle2 =
+      'Get your products delivered to your doorstep quickly and reliably.';
+  static const String tOnboardingSubTitle3 =
+      'Pay for your products easily and securely through our app.';
+}
