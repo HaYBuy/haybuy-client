f2a24856227267c871a49b1ea138b2ea1fb6da30
Setsiri <setsiriaunsiam@gmail.com>
2025-10-01 23:56:16 +0700

    edit app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:haybuy_client/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:haybuy_client/utils/theme/theme.dart';
 
-void main() {
-
-  
+void main() async {
+  await GetStorage.init();
 
   runApp(const App());
 }
@@ -14,7 +15,7 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
