d78e1ec8435d34e0eb84a456361f74125bbb71b8
adithep.b <adithepbaebmueann@gmail.com>
2025-10-02 01:40:34 +0700

    feat: relate signup page and verify email page

---
+++ lib/features/authentication/screens/signup/verify_email.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:haybuy_client/utils/constants/sizes.dart';
+
+class VerifyEmailScreen extends StatelessWidget {
+  const VerifyEmailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () => Get.back(),
+            icon: const Icon(CupertinoIcons.clear),
+          ),
+        ],
+      ),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(Sizes.defaultSpace),
+          child: Column(
+            children: [
+              ///Image
+
+              ///Title & Subtitle
+
+              ///Button
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
