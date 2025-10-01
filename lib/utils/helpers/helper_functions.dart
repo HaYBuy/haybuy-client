f181151811ac33ca7c96d4c956e04eb9303d5e3a
Setsiri <setsiriaunsiam@gmail.com>
2025-10-01 16:53:58 +0700

    get device Info and fix help func

--- lib/utils/helpers/helper_functions.dart
+++ lib/utils/helpers/helper_functions.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class THelperFunctions {
+  
   static Size screenSize() {
     return MediaQuery.of(Get.context!).size;
   }
 
-  static double screenHeight() {
-    return MediaQuery.of(Get.context!).size.height;
+  static double screenHeight(BuildContext context) {
+    return MediaQuery.of(context).size.height;
   }
 
-  static double screenWidth() {
-    return MediaQuery.of(Get.context!).size.width;
+  static double screenWidth(BuildContext context) {
+    return MediaQuery.of(context).size.width;
   }
 }
