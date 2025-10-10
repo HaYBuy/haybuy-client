c737c7b2d17f50a8516f2bb81ef7fa7ec21ad684
Setsiri <setsiriaunsiam@gmail.com>
2025-10-10 20:16:45 +0700

    add image and path image

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -2,8 +2,8 @@ class Images {
   static const String darkAppLogo = "assets/logos/haybuy_logo.png";
   static const String lightAppLogo = "assets/logos/haybuy_logo.png";
 
-  static const String google = "assets/images/dark_app_logo.png";
-  static const String facebook = "assets/images/dark_app_logo.png";
+  static const String google = "assets/logos/google-logo.png";
+  static const String facebook = "assets/logos/facebook-logo.png";
 
   static const String onboardingImage1 =
       'assets/images/on_boarding_images/on-boarding-image-1.png';
@@ -18,4 +18,8 @@ class Images {
       "assets/images/on_boarding_images/sammy-line-man-receives-a-mail.png";
 
   static const String user = "assets/logos/haybuy_logo.png";
+
+  static const String creditCard = "assets/icons/payment_methods/credit-card.png";
+  static const String mastercard = "assets/icons/payment_methods/master-card.png";
+  static const String successfulPayment = "assets/icons/payment_methods/successful-payment-icon.png";
 }
