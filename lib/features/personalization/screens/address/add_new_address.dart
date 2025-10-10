b7400718a82eb67ee5740c120e576692eedc5f38
adithep.b <adithepbaebmueann@gmail.com>
2025-10-10 13:27:45 +0700

    feat: add address page ,add address page and add navigation from setting page to address page

---
+++ lib/features/personalization/screens/address/add_new_address.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:haybuy_client/common/widgets/appbar/appbar.dart';
+import 'package:haybuy_client/utils/constants/sizes.dart';
+import 'package:iconsax/iconsax.dart';
+
+class AddNewAddressScreen extends StatelessWidget {
+  const AddNewAddressScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(showBackArrow: true, title: Text('Add new Address')),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(Sizes.defaultSpace),
+          child: Form(
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Iconsax.user),
+                    labelText: 'Name',
+                  ),
+                ),
+                const SizedBox(height: Sizes.spaceBtwInputFields),
+                TextFormField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Iconsax.mobile),
+                    labelText: 'Phone Number',
+                  ),
+                ),
+                const SizedBox(height: Sizes.spaceBtwInputFields),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Iconsax.building_31),
+                          labelText: 'Street',
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: Sizes.spaceBtwInputFields),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Iconsax.code),
+                          labelText: 'Postal code',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: Sizes.spaceBtwInputFields),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Iconsax.building),
+                          labelText: 'City',
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: Sizes.spaceBtwInputFields),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Iconsax.activity),
+                          labelText: 'State',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: Sizes.spaceBtwInputFields),
+                TextFormField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Iconsax.global),
+                    labelText: 'Country',
+                  ),
+                ),
+                const SizedBox(height: Sizes.defaultSpace),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(onPressed: () {}, child: Text('Save')),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/personalization/screens/address/widgets/single_address.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:haybuy_client/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:haybuy_client/utils/constants/colors.dart';
+import 'package:haybuy_client/utils/constants/sizes.dart';
+import 'package:haybuy_client/utils/helpers/helper_function.dart';
+import 'package:iconsax/iconsax.dart';
+
+class SingleAddress extends StatelessWidget {
+  const SingleAddress({super.key, required this.selectedAddress});
+
+  final bool selectedAddress;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = HelperFunctions.isDarkMode(context);
+    return RoundedContainer(
+      showBorder: true,
+      padding: const EdgeInsets.all(Sizes.md),
+      width: double.infinity,
+      backgroundColor: selectedAddress
+          ? ConstColors.primary.withOpacity(0.5)
+          : Colors.transparent,
+      borderColor: selectedAddress
+          ? Colors.transparent
+          : dark
+          ? ConstColors.darkGrey
+          : ConstColors.grey,
+      margin: const EdgeInsets.only(bottom: Sizes.spaceBtwItems),
+      child: Stack(
+        children: [
+          Positioned(
+            right: 5,
+            top: 0,
+            child: Icon(
+              selectedAddress ? Iconsax.tick_circle : null,
+              color: selectedAddress
+                  ? dark
+                        ? ConstColors.lightGrey
+                        : ConstColors.dark
+                  : null,
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Jane Doe',
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(height: Sizes.sm / 2),
+              const Text(
+                '+66 1234 56789',
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              const SizedBox(height: Sizes.sm / 2),
+              const Text(
+                '111/01, Paitong, Betong, Yala, 95110',
+                softWrap: true,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
