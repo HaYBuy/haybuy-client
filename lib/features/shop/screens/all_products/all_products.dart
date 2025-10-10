3628e71789172f01a908c4349d97c929de836f19
Setsiri <setsiriaunsiam@gmail.com>
2025-10-10 22:19:12 +0700

    all product screen

--- lib/features/shop/screens/all_products/all_products.dart
+++ lib/features/shop/screens/all_products/all_products.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:haybuy_client/common/widgets/appbar/appbar.dart';
+import 'package:haybuy_client/common/widgets/layouts/grid_layout.dart';
+import 'package:haybuy_client/common/widgets/products/product_cards/product_card_vertical.dart';
 import 'package:haybuy_client/utils/constants/sizes.dart';
 import 'package:iconsax/iconsax.dart';
 
@@ -15,12 +17,17 @@ class AllProducts extends StatelessWidget {
           padding: EdgeInsets.all(Sizes.defaultSpace),
           child: Column(
             children: [
-              //Dropdown
+              //Dropdown for sorting
               DropdownButtonFormField(
                 decoration: InputDecoration(prefixIcon: Icon(Iconsax.sort)),
                 onChanged: (value) {},
-                items: [],
-              )
+                items: ['Name', 'Price: Low to High', 'Price: High to Low', 'Newest', 'Popularity', 'Sales']
+                    .map((e) => DropdownMenuItem(value: e, child: Text(e)))
+                    .toList(),
+              ),
+              const SizedBox(height: Sizes.spaceBtwSections),
+              //Products Grid
+              GridLayout(itemCount: 4, itemBuilder: (_, index) => const ProductCardVertical()),
             ],
           )
         )
